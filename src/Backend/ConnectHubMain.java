c434617001b06af46c4a25ba7a2e00e8bf0462b8
BeedoProgrammer <abdelrahmanh38@gmail.com>
2024-12-01 14:33:27 +0200

    Create User class

---
+++ src/Backend/ConnectHubMain.java
@@ -0,0 +1,8 @@
+package Backend;
+
+public class ConnectHubMain {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }  
+}
---
+++ src/Backend/User.java
@@ -0,0 +1,74 @@
+package Backend;
+
+public class User {
+    private long userId;
+    private String email;
+    private String username;
+    private String password;
+    private String dateOfBirth;
+    private boolean status;
+
+    public User(long userId, String email, String username, String password, String dateOfBirth, boolean status) {
+        this.userId = userId;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.status = status;
+    }
+    
+    public void changeStatus(){
+        if(status)
+            status = false;
+        else
+            status = true;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
