b4d844b0ec0e3793bea085e56c9d22170841e3ce
youssef <youssefyoussef.m4@gmail.com>
2024-12-02 00:49:11 +0200

    Add basic database Classes

---
+++ src/Backend/Database.java
@@ -0,0 +1,18 @@
+package Backend;
+
+public abstract class Database {
+    private String fileName;
+
+    public Database(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+    
+}
