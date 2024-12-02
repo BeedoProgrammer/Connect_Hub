2743fcfbceaf5afa6aad0e231c276c8f6f9d16e6
Omar Waleed <omarwaleedelsayed2244@gmail.com>
2024-12-02 10:07:58 +0200

    Created an enum for relationships between users

---
+++ src/Backend/FriendshipStatus.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+/**
+ *
+ * @author user
+ */
+public enum FriendshipStatus {
+    PENDING,
+    ACCEPTED,
+    BLOCKED
+}
