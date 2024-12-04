f4ddf1c915f5d1bccf401cde0c491b1077a51237
youssef <youssefyoussef.m4@gmail.com>
2024-12-05 00:57:26 +0200

    Use gson to parse JSONString to JSONObject then hashmap to geth user relations from json file

--- src/Backend/UserDatabase.java
+++ src/Backend/UserDatabase.java
@@ -1,6 +1,7 @@
 package Backend;
 
 
+import com.google.gson.Gson;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -30,27 +31,33 @@ public class UserDatabase extends Database {
         }
     }
     private User getUserFromMap(Map<String,Object> mapOfUser) {
-        long userId = (long)mapOfUser.get("userId");
-            String email = (String)mapOfUser.get("email");
-            String userName = (String)mapOfUser.get("userName");
-            char[] password = (char[])mapOfUser.get("password");
-            LocalDate dateOfBirth = (LocalDate)mapOfUser.get("dateOfBirth");
-            boolean status = (boolean)mapOfUser.get("status");
-            HashMap<Long,FriendshipStatus> relationships = (HashMap<Long,FriendshipStatus>)mapOfUser.get("status");
-            User tempUser = new User(userId, email, userName, password, dateOfBirth, status);
-            tempUser.setRelationships(relationships);
-            return tempUser;
+        long userId = (long)mapOfUser.get("userId");                // saved in json as long
+        String email = (String)mapOfUser.get("email");              // saved in json as string
+        String userName = (String)mapOfUser.get("userName");        // saved in json as string
+        String password = (String)mapOfUser.get("password");        // saved in json as string
+        String dateOfBirth = (String)mapOfUser.get("dateOfBirth");  // saved in json as string
+        boolean status = (boolean)mapOfUser.get("status");          // saved in json as boolean
+        // parsing string into hashmap
+            Gson gson = new Gson();
+            String jsonString = (String)mapOfUser.get("relationships"); // saved in json as string (JSONString)
+            JSONObject jsonObject = gson.fromJson(jsonString, JSONObject.class);
+            HashMap<Long,FriendshipStatus> relationships = (HashMap<Long,FriendshipStatus>) jsonObject; // casting from JSONObject to hashmap
+        //
+        User tempUser = new User(userId, email, userName, password.toCharArray(), LocalDate.parse(dateOfBirth), status);
+        tempUser.setRelationships(relationships);
+        return tempUser;
     }
     // impelement method to get user as hashmap??
     private Map<String,Object> getMapFromUser(User user) {
         Map<String,Object> tempUserMap = new HashMap<>();
-        tempUserMap.put("userId", String.valueOf(user.getUserId()));
-        tempUserMap.put("email", user.getEmail());
-        tempUserMap.put("userName", user.getUsername());
-        tempUserMap.put("password", new String(user.getPassword()));
-        tempUserMap.put("dateOfBirth", user.getDateOfBirth().toString());
-        tempUserMap.put("status", String.valueOf(user.isStatus())); // isStatus?? not getStatus or isOnline??
-        tempUserMap.put("relationships", user.getRelationships());
+        tempUserMap.put("userId", user.getUserId());                        // saved in json as long
+        tempUserMap.put("email", user.getEmail());                          // saved in json as string
+        tempUserMap.put("userName", user.getUsername());                    // saved in json as string  
+        tempUserMap.put("password", new String(user.getPassword()));        // saved in json as string
+        tempUserMap.put("dateOfBirth", user.getDateOfBirth().toString());   // saved in json as string
+        tempUserMap.put("status", user.isStatus());                         // saved in json as boolean
+        JSONObject relation = new JSONObject(user.getRelationships());
+        tempUserMap.put("relationships", relation.toJSONString());          // saved in json as string (JSONString)
         return tempUserMap;
     }
     public void saveToFile() throws FileNotFoundException, IOException, ParseException {    // saves useres arraylist into json format file
