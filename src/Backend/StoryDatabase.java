2a0b11a275df12094c4bb17b09bee1820b3fbb60
youssef <youssefyoussef.m4@gmail.com>
2024-12-05 20:41:56 +0200

    Update StoryDatabase.java

--- src/Backend/StoryDatabase.java
+++ src/Backend/StoryDatabase.java
@@ -16,56 +16,72 @@ import org.json.simple.parser.ParseException;
 
 
 public class StoryDatabase extends Database {
-    private ArrayList<Story> stories;
+//    private ArrayList<Story> stories;
+    
+    private static StoryDatabase instance = null;
+    
+    public static StoryDatabase getInstance() {
+        if (instance == null) {
+            instance = new StoryDatabase("stories.json");
+        }
+        return instance;
+    }
 
-    public StoryDatabase(String fileName) {
+    private StoryDatabase(String fileName) {
         super(fileName);
-        stories = new ArrayList<>();
+//        stories = new ArrayList<>();
     }
 
-    public void readFromFile() throws FileNotFoundException, IOException, ParseException {    // modifies stories arraylist
-        this.stories.clear(); // remove all previous elements from list
-        JSONArray jsonStories = new JSONArray();
-        jsonStories = (JSONArray) new JSONParser().parse(new FileReader(getFileName()));
-        // parse each element in the jsonArray into a story object
-        for (int i = 0; i < jsonStories.size(); i++) {
-            stories.add(getStoryFromMap((Map)jsonStories.get(i)));
-        }
+//    public void readFromFile() throws FileNotFoundException, IOException, ParseException {    // modifies stories arraylist
+//        this.stories.clear(); // remove all previous elements from list
+//        JSONArray jsonStories = new JSONArray();
+//        jsonStories = (JSONArray) new JSONParser().parse(new FileReader(getFileName()));
+//        // parse each element in the jsonArray into a story object
+//        for (int i = 0; i < jsonStories.size(); i++) {
+//            stories.add(getStoryFromMap((Map)jsonStories.get(i)));
+//        }
+//    }
+    @Override
+    protected Story getRecordFromMap(Map<String,Object> mapOfStory) {
+    long contentId = (long)mapOfStory.get("contentId");
+    long authorId = (long)mapOfStory.get("authorId");
+    String timestamp = (String)mapOfStory.get("timestamp");
+    String contentString = (String)mapOfStory.get("contentString");
+    String contentImagePath = (String)mapOfStory.get("contentImagePath");
+    Story tempStory = new Story(contentId, authorId, contentString, contentImagePath);
+    tempStory.setTimestamp(LocalDateTime.parse(timestamp));
+    return tempStory;
     }
-    private Story getStoryFromMap(Map<String,Object> mapOfStory) {
-        long contentId = (long)mapOfStory.get("contentId");
-        long authorId = (long)mapOfStory.get("authorId");
-        String timestamp = (String)mapOfStory.get("timestamp");
-        String contentString = (String)mapOfStory.get("contentString");
-        String contentImagePath = (String)mapOfStory.get("contentImagePath");
-        Story tempStory = new Story(contentId, authorId, contentString, contentImagePath);
-        tempStory.setTimestamp(LocalDateTime.parse(timestamp));
-        return tempStory;
-    }
-    public void saveToFile() throws FileNotFoundException, IOException, ParseException {    // saves stories arraylist into json format file
-        JSONArray jsonStories = new JSONArray();
-        for (int i = 0; i < stories.size(); i++) {
-            jsonStories.add(getMapFromStory(stories.get(i)));
-        }
-        PrintWriter pw = new PrintWriter(getFileName()); 
-        pw.write(jsonStories.toJSONString()); 
-        pw.flush(); 
-        pw.close(); 
-    }
-    private Map<String,Object> getMapFromStory(Story story) {
+//    public void saveToFile() throws FileNotFoundException, IOException, ParseException {    // saves stories arraylist into json format file
+//        JSONArray jsonStories = new JSONArray();
+//        for (int i = 0; i < stories.size(); i++) {
+//            jsonStories.add(getMapFromStory(stories.get(i)));
+//        }
+//        PrintWriter pw = new PrintWriter(getFileName()); 
+//        pw.write(jsonStories.toJSONString()); 
+//        pw.flush(); 
+//        pw.close(); 
+//    }
+    @Override
+    protected Map<String,Object> getMapFromRecord(Object story) {
         Map<String,Object> tempStoryMap = new HashMap<>();
-        tempStoryMap.put("contentId", story.getContentId());
-        tempStoryMap.put("authorId", story.getAuthorId());
-        tempStoryMap.put("timestamp", story.getTimestamp().toString());
-        tempStoryMap.put("contentString", story.getContentString());
-        tempStoryMap.put("contentImagePath", story.getContentImagePath());
+        Story tempStory = (Story)story;
+        tempStoryMap.put("contentId", tempStory.getContentId());
+        tempStoryMap.put("authorId", tempStory.getAuthorId());
+        tempStoryMap.put("timestamp", tempStory.getTimestamp().toString());
+        tempStoryMap.put("contentString", tempStory.getContentString());
+        tempStoryMap.put("contentImagePath", tempStory.getContentImagePath());
         return tempStoryMap;
     }
     public void addStory(Story story) throws IOException, FileNotFoundException, ParseException {
-        stories.add(story);
-        saveToFile();
+        super.addRecord(story);
     }
     public ArrayList<Story> getStories() {
+        ArrayList<Object> tempObjects = super.getRecords();
+        ArrayList<Story> stories = new ArrayList<>();
+        for (Object tempObject : tempObjects) {
+            stories.add((Story)tempObject);
+        }
         return stories;
     }
 
