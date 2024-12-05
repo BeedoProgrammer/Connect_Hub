87cbbc93637c7a55bf3dc275c55368647b0a5a92
youssef <youssefyoussef.m4@gmail.com>
2024-12-05 20:40:30 +0200

    Impalement Singleton and template design patterns

--- src/Backend/PostDatabase.java
+++ src/Backend/PostDatabase.java
@@ -16,23 +16,33 @@ import java.util.ArrayList;
 
 
 public class PostDatabase extends Database {
-    private ArrayList<Post> posts;
-
-    public PostDatabase(String fileName) {
-        super(fileName);
-        posts = new ArrayList<>();
-    }
-
-    public void readFromFile() throws FileNotFoundException, IOException, ParseException {    // modifies posts arraylist
-        this.posts.clear(); // remove all previous elements from list
-        JSONArray jsonPosts = new JSONArray();
-        jsonPosts = (JSONArray) new JSONParser().parse(new FileReader(getFileName()));
-        // parse each element in the jsonArray into a Post object
-        for (int i = 0; i < jsonPosts.size(); i++) {
-            posts.add(getPostFromMap((Map)jsonPosts.get(i)));
+//    private ArrayList<Post> posts;
+    
+    private static PostDatabase instance = null;
+    
+    public static PostDatabase getInstance() {
+        if (instance == null) {
+            instance = new PostDatabase("posts.json");
         }
+        return instance;
     }
-    private Post getPostFromMap(Map<String,Object> mapOfPost) {
+
+    private PostDatabase(String fileName) {
+        super(fileName);
+//        posts = new ArrayList<>();
+    }
+
+//    public void readFromFile() throws FileNotFoundException, IOException, ParseException {    // modifies posts arraylist
+//        this.posts.clear(); // remove all previous elements from list
+//        JSONArray jsonPosts = new JSONArray();
+//        jsonPosts = (JSONArray) new JSONParser().parse(new FileReader(getFileName()));
+//        // parse each element in the jsonArray into a Post object
+//        for (int i = 0; i < jsonPosts.size(); i++) {
+//            posts.add(getPostFromMap((Map)jsonPosts.get(i)));
+//        }
+//    }
+    @Override
+    protected Post getRecordFromMap(Map<String,Object> mapOfPost) {
         long contentId = (long)mapOfPost.get("contentId");
         long authorId = (long)mapOfPost.get("authorId");
         String timestamp = (String)mapOfPost.get("timestamp");
@@ -42,30 +52,35 @@ public class PostDatabase extends Database {
         tempPost.setTimestamp(LocalDateTime.parse(timestamp));
         return tempPost;
     }
-    public void saveToFile() throws FileNotFoundException, IOException, ParseException {    // saves posts arraylist into json format file
-        JSONArray jsonPosts = new JSONArray();
-        for (int i = 0; i < posts.size(); i++) {
-            jsonPosts.add(getMapFromPost(posts.get(i)));
-        }
-        PrintWriter pw = new PrintWriter(getFileName()); 
-        pw.write(jsonPosts.toJSONString()); 
-        pw.flush(); 
-        pw.close(); 
-    }
-    private Map<String,Object> getMapFromPost(Post post) {
+//    public void saveToFile() throws FileNotFoundException, IOException, ParseException {    // saves posts arraylist into json format file
+//        JSONArray jsonPosts = new JSONArray();
+//        for (int i = 0; i < posts.size(); i++) {
+//            jsonPosts.add(getMapFromPost(posts.get(i)));
+//        }
+//        PrintWriter pw = new PrintWriter(getFileName()); 
+//        pw.write(jsonPosts.toJSONString()); 
+//        pw.flush(); 
+//        pw.close(); 
+//    }
+    protected Map<String,Object> getMapFromRecord(Object post) {
         Map<String,Object> tempPostMap = new HashMap<>();
-        tempPostMap.put("contentId", post.getContentId());
-        tempPostMap.put("authorId", post.getAuthorId());
-        tempPostMap.put("timestamp", post.getTimestamp().toString());
-        tempPostMap.put("contentString", post.getContentString());
-        tempPostMap.put("contentImagePath", post.getContentImagePath());
+        Post tempPost = (Post)post;
+        tempPostMap.put("contentId", tempPost.getContentId());
+        tempPostMap.put("authorId", tempPost.getAuthorId());
+        tempPostMap.put("timestamp", tempPost.getTimestamp().toString());
+        tempPostMap.put("contentString", tempPost.getContentString());
+        tempPostMap.put("contentImagePath", tempPost.getContentImagePath());
         return tempPostMap;
     }
     public void addPost(Post post) throws IOException, FileNotFoundException, ParseException {
-        posts.add(post);
-        saveToFile();
+        super.addRecord(post);
     }
     public ArrayList<Post> getPosts() {
+        ArrayList<Object> tempObjects = super.getRecords();
+        ArrayList<Post> posts = new ArrayList<>();
+        for (Object tempObject : tempObjects) {
+            posts.add((Post)tempObject);
+        }
         return posts;
     }
 
--- src/Backend/UserDatabase.java
+++ src/Backend/UserDatabase.java
@@ -15,27 +15,40 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 public class UserDatabase extends Database {
-    private ArrayList<User> users;
-
-    public UserDatabase(String fileName) {
-        super(fileName);
-        this.users = new ArrayList<>();
-    }
-    public void readFromFile() throws FileNotFoundException, IOException, ParseException {    // modifies users arraylist
-        this.users.clear(); // remove all previous elements from list
-        JSONArray jsonUsers = new JSONArray();
-        jsonUsers = (JSONArray) new JSONParser().parse(new FileReader(getFileName()));
-        // parse each element in the jsonArray into a user object
-        for (int i = 0; i < jsonUsers.size(); i++) {
-            users.add(getUserFromMap((Map)jsonUsers.get(i)));
+//    private ArrayList<User> users;
+    
+    private static UserDatabase instance = null;
+    
+    public static UserDatabase getInstance() {
+        if (instance == null) {
+            instance = new UserDatabase("users.json");
         }
+        return instance;
     }
-    private User getUserFromMap(Map<String,Object> mapOfUser) {
+    
+    private UserDatabase(String fileName) {
+        super(fileName);
+//        this.users = new ArrayList<>();
+    }
+//    public void readFromFile() throws FileNotFoundException, IOException, ParseException {    // modifies users arraylist
+//        this.users.clear(); // remove all previous elements from list
+//        JSONArray jsonUsers = new JSONArray();
+//        jsonUsers = (JSONArray) new JSONParser().parse(new FileReader(getFileName()));
+//        // parse each element in the jsonArray into a user object
+//        for (int i = 0; i < jsonUsers.size(); i++) {
+//            users.add(getUserFromMap((Map)jsonUsers.get(i)));
+//        }
+//    }
+    @Override
+    protected User getRecordFromMap(Map<String,Object> mapOfUser) {
         long userId = (long)mapOfUser.get("userId");                // saved in json as long
         String email = (String)mapOfUser.get("email");              // saved in json as string
         String userName = (String)mapOfUser.get("userName");        // saved in json as string
         String password = (String)mapOfUser.get("password");        // saved in json as string
         String dateOfBirth = (String)mapOfUser.get("dateOfBirth");  // saved in json as string
+        String bio = (String)mapOfUser.get("bio");
+        String coverPic = (String)mapOfUser.get("coverPic");
+        String profilePic = (String)mapOfUser.get("profilePic");
         boolean status = (boolean)mapOfUser.get("status");          // saved in json as boolean
         // parsing string into hashmap
             Gson gson = new Gson();
@@ -43,44 +56,62 @@ public class UserDatabase extends Database {
             JSONObject jsonObject = gson.fromJson(jsonString, JSONObject.class);
             HashMap<Long,FriendshipStatus> relationships = (HashMap<Long,FriendshipStatus>) jsonObject; // casting from JSONObject to hashmap
         //
-        User tempUser = new User(userId, email, userName, password.toCharArray(), LocalDate.parse(dateOfBirth), status);
+        User tempUser = new User.UserBuilder(userId, email, userName, password.toCharArray(), LocalDate.parse(dateOfBirth), status)
+            .bio(bio)
+            .coverPhoto(coverPic)
+            .profilePic(profilePic)
+            .build();
+//        User tempUser = new User(userId, email, userName, password.toCharArray(), LocalDate.parse(dateOfBirth), status);
         tempUser.setRelationships(relationships);
+        
         return tempUser;
     }
-    // impelement method to get user as hashmap??
-    private Map<String,Object> getMapFromUser(User user) {
+    @Override
+    protected Map<String,Object> getMapFromRecord(Object user) {
         Map<String,Object> tempUserMap = new HashMap<>();
-        tempUserMap.put("userId", user.getUserId());                        // saved in json as long
-        tempUserMap.put("email", user.getEmail());                          // saved in json as string
-        tempUserMap.put("userName", user.getUsername());                    // saved in json as string  
-        tempUserMap.put("password", new String(user.getPassword()));        // saved in json as string
-        tempUserMap.put("dateOfBirth", user.getDateOfBirth().toString());   // saved in json as string
-        tempUserMap.put("status", user.isStatus());                         // saved in json as boolean
-        JSONObject relation = new JSONObject(user.getRelationships());
-        tempUserMap.put("relationships", relation.toJSONString());          // saved in json as string (JSONString)
+        User tempUser = (User)user;
+        tempUserMap.put("userId", tempUser.getUserId());                        // saved in json as long
+        tempUserMap.put("email", tempUser.getEmail());                          // saved in json as string
+        tempUserMap.put("userName", tempUser.getUsername());                    // saved in json as string  
+        
+        tempUserMap.put("password", new String(tempUser.getPassword()));        // saved in json as string
+        
+        tempUserMap.put("dateOfBirth", tempUser.getDateOfBirth().toString());   // saved in json as string
+        tempUserMap.put("bio", tempUser.getBio());                              // saved in json as string
+        tempUserMap.put("coverPic", tempUser.getCoverPhoto());                  // saved in json as string
+        tempUserMap.put("profilePic", tempUser.getProfilePic());                // saved in json as string
+        tempUserMap.put("status", tempUser.isStatus());                         // saved in json as boolean
+
+        JSONObject relation = new JSONObject(tempUser.getRelationships());
+        tempUserMap.put("relationships", relation.toJSONString());              // saved in json as string (JSONString)
         return tempUserMap;
     }
-    public void saveToFile() throws FileNotFoundException, IOException, ParseException {    // saves useres arraylist into json format file
-        JSONArray jsonUsers = new JSONArray();
-        for (int i = 0; i < users.size(); i++) {
-            jsonUsers.add(getMapFromUser(users.get(i)));
-        }
-        PrintWriter pw = new PrintWriter(getFileName()); 
-        pw.write(jsonUsers.toJSONString()); 
-        pw.flush(); 
-        pw.close(); 
-    }
+//    public void saveToFile() throws FileNotFoundException, IOException, ParseException {    // saves users arraylist into json format file
+//        JSONArray jsonUsers = new JSONArray();
+//        for (int i = 0; i < users.size(); i++) {
+//            jsonUsers.add(getMapFromUser(users.get(i)));
+//        }
+//        PrintWriter pw = new PrintWriter(getFileName()); 
+//        pw.write(jsonUsers.toJSONString()); 
+//        pw.flush(); 
+//        pw.close(); 
+//    }
     public void addUser(User user) throws IOException, FileNotFoundException, ParseException {
-        users.add(user);
-        saveToFile();
+        super.addRecord(user);
     }
     public ArrayList<User> getUsers() {
+        ArrayList<Object> tempObjects = super.getRecords();
+        ArrayList<User> users = new ArrayList<>();
+        for (Object tempObject : tempObjects) {
+            users.add((User)tempObject);
+        }
         return users;
     }
     public User getUserFromId(long userId) {    // returns user object if found and null if not found
-        for (int i = 0; i < users.size(); i++) {
-            if (userId == users.get(i).getUserId()) {
-                return users.get(i);
+        ArrayList<Object> tempObjects = super.getRecords();
+        for (int i = 0; i < tempObjects.size(); i++) {
+            if (userId == ((User)tempObjects.get(i)).getUserId()) {
+                return (User)tempObjects.get(i);
             }
         }
         return null;
