74821a590b9b923ca2db271fc943fb1476ee0131
youssef <youssefyoussef.m4@gmail.com>
2024-12-06 16:06:25 +0200

    Remove unnecessary comments and make adding records automatically save to json file

--- src/Backend/PostDatabase.java
+++ src/Backend/PostDatabase.java
@@ -29,18 +29,7 @@ public class PostDatabase extends Database {
 
     private PostDatabase(String fileName) {
         super(fileName);
-//        posts = new ArrayList<>();
     }
-
-//    public void readFromFile() throws FileNotFoundException, IOException, ParseException {    // modifies posts arraylist
-//        this.posts.clear(); // remove all previous elements from list
-//        JSONArray jsonPosts = new JSONArray();
-//        jsonPosts = (JSONArray) new JSONParser().parse(new FileReader(getFileName()));
-//        // parse each element in the jsonArray into a Post object
-//        for (int i = 0; i < jsonPosts.size(); i++) {
-//            posts.add(getPostFromMap((Map)jsonPosts.get(i)));
-//        }
-//    }
     @Override
     protected Post getRecordFromMap(Map<String,Object> mapOfPost) {
         long contentId = (long)mapOfPost.get("contentId");
@@ -52,16 +41,6 @@ public class PostDatabase extends Database {
         tempPost.setTimestamp(LocalDateTime.parse(timestamp));
         return tempPost;
     }
-//    public void saveToFile() throws FileNotFoundException, IOException, ParseException {    // saves posts arraylist into json format file
-//        JSONArray jsonPosts = new JSONArray();
-//        for (int i = 0; i < posts.size(); i++) {
-//            jsonPosts.add(getMapFromPost(posts.get(i)));
-//        }
-//        PrintWriter pw = new PrintWriter(getFileName()); 
-//        pw.write(jsonPosts.toJSONString()); 
-//        pw.flush(); 
-//        pw.close(); 
-//    }
     protected Map<String,Object> getMapFromRecord(Object post) {
         Map<String,Object> tempPostMap = new HashMap<>();
         Post tempPost = (Post)post;
