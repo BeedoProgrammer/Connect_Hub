2ff89d8486cee7b7654a64e04cbdc8b02095e343
Omar Waleed <omarwaleedelsayed2244@gmail.com>
2024-12-10 23:57:58 +0200

    added group search button option in general search place

--- src/Frontend/SearchBarWindow.java
+++ src/Frontend/SearchBarWindow.java
@@ -5,6 +5,8 @@
 package Frontend;
 
 import Backend.FriendshipStatus;
+import Backend.Group;
+import Backend.GroupSearch;
 import Backend.User;
 import Backend.UserSearch;
 import java.time.LocalDate;
@@ -16,25 +18,35 @@ import java.util.ArrayList;
  */
 public class SearchBarWindow extends javax.swing.JFrame {
 private UserSearch SearchOption;
+private GroupSearch GroupOption;
    private User CurrentUser;
-    public SearchBarWindow(User CurrentUser,ArrayList <User> AllUsers) {
+    public SearchBarWindow(User CurrentUser,ArrayList <User> AllUsers,ArrayList <Group> AllGroups) {
         initComponents();
        this.CurrentUser=CurrentUser;
        this.SearchOption=new UserSearch(CurrentUser,AllUsers);
+       this.GroupOption=new GroupSearch(CurrentUser,AllGroups);
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        Search = new javax.swing.JButton();
+        UserSearch = new javax.swing.JButton();
         SearchInput = new javax.swing.JTextField();
+        GroupSearch = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        Search.setText("Search");
-        Search.addActionListener(new java.awt.event.ActionListener() {
+        UserSearch.setText("User Search");
+        UserSearch.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                SearchActionPerformed(evt);
+                UserSearchActionPerformed(evt);
+            }
+        });
+
+        GroupSearch.setText("GroupSearch");
+        GroupSearch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                GroupSearchActionPerformed(evt);
             }
         });
 
@@ -43,10 +55,12 @@ private UserSearch SearchOption;
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(207, Short.MAX_VALUE)
+                .addContainerGap(42, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(Search)
+                        .addComponent(GroupSearch)
+                        .addGap(63, 63, 63)
+                        .addComponent(UserSearch)
                         .addGap(105, 105, 105))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(SearchInput, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -58,32 +72,43 @@ private UserSearch SearchOption;
                 .addGap(74, 74, 74)
                 .addComponent(SearchInput, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 79, Short.MAX_VALUE)
-                .addComponent(Search)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(UserSearch)
+                    .addComponent(GroupSearch))
                 .addGap(91, 91, 91))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void SearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SearchActionPerformed
+    private void UserSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UserSearchActionPerformed
  String searchTerm = SearchInput.getText();
     SearchOption.DesiredPeople(searchTerm);
 
     SearchResultsWindow resultsWindow = new SearchResultsWindow(CurrentUser, SearchOption);
     resultsWindow.setVisible(true);
-    }//GEN-LAST:event_SearchActionPerformed
+    }//GEN-LAST:event_UserSearchActionPerformed
+
+    private void GroupSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GroupSearchActionPerformed
+      String searchTerm = SearchInput.getText();
+      GroupOption.DesiredGroups(searchTerm);
+      GroupSearchresultsWindow resultsWindow = new GroupSearchresultsWindow(CurrentUser, GroupOption);
+    }//GEN-LAST:event_GroupSearchActionPerformed
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        Group g1=new Group("sens","hello");
+         Group g2=new Group("senso","hello");
+          Group g3=new Group("sen","hello");
          User user1 = new User.UserBuilder(1, "john.doe@example.com", "john_doe", "password123".toCharArray(), LocalDate.of(1995, 5, 15), true)
                 .bio("Just a regular guy who loves coding!")
-                .profilePic("john_profile_pic.jpg")
+                .profilePic("john_profile_pic.jpg").addGroup(g1)
                 .coverPhoto("john_cover.jpg")
                 .build();
 
-        User user2 = new User.UserBuilder(2, "jane.smith@example.com", "mike_smith", "password456".toCharArray(), LocalDate.of(1997, 8, 20), true)
+        User user2 = new User.UserBuilder(2, "jane.smith@example.com", "mike_ali", "password456".toCharArray(), LocalDate.of(1997, 8, 20), true)
                 .bio("Adventurer, photographer, and foodie.")
                 .profilePic("jane_profile_pic.jpg")
                 .coverPhoto("jane_cover.jpg")
@@ -97,19 +122,24 @@ private UserSearch SearchOption;
         user1.addRelationship(3, FriendshipStatus.ACCEPTED);   // user1 sent request to user3
         user3.addRelationship(1, FriendshipStatus.ACCEPTED);         // user2 and user3 are friends
  ArrayList<User> systemUsers = new ArrayList<>();
+ ArrayList<Group> systemGroups=new ArrayList<>();
+ systemGroups.add(g1);
+  systemGroups.add(g2);
+   systemGroups.add(g3);
         systemUsers.add(user1);
         systemUsers.add(user2);
         systemUsers.add(user3);
         // List of all users in the system
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new SearchBarWindow(user1,systemUsers).setVisible(true);
+                new SearchBarWindow(user1,systemUsers,systemGroups).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Search;
+    private javax.swing.JButton GroupSearch;
     private javax.swing.JTextField SearchInput;
+    private javax.swing.JButton UserSearch;
     // End of variables declaration//GEN-END:variables
 }
