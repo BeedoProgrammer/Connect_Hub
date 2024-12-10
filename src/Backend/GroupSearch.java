127be0d07df2d1b7810e62ace0302bb5b0190847
Omar Waleed <omarwaleedelsayed2244@gmail.com>
2024-12-11 00:01:40 +0200

    implmeneted group search result algorithm can change depending on new requirements set

---
+++ src/Backend/GroupSearch.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author user
+ */
+public class GroupSearch {
+      private User CurrentUser;
+  private  ArrayList <Group> SystemGroups; ///// all groups in system
+   private   ArrayList<Group> MyDesiredGroups;  ///// groups with this search
+    public GroupSearch (User CurrentUser ,ArrayList<Group> SystemGroups)
+    {
+        this.CurrentUser=CurrentUser;
+        this.SystemGroups=SystemGroups;
+        MyDesiredGroups=new ArrayList<>();
+    }
+    public void DesiredGroups(String searchName) {
+    
+        for (Group group : SystemGroups) {
+            if (group.getName().toLowerCase().contains(searchName.toLowerCase())) {
+                MyDesiredGroups.add(group);
+            }
+        }
+  }  
+     public ArrayList<Group> joinedGroups() {
+    ArrayList<Group> joinedGroups = new ArrayList<>();
+    for (Group group : MyDesiredGroups) {
+        if (CurrentUser.getMygroups().contains(group)) {
+            joinedGroups.add(group);
+        }
+    }
+    return joinedGroups;
+}
+public ArrayList<Group> unjoinedGroups() {
+    ArrayList<Group> unjoinedGroups = new ArrayList<>();
+    for (Group group : MyDesiredGroups) {
+        if (!CurrentUser.getMygroups().contains(group)) {
+            unjoinedGroups.add(group);
+        }
+    }
+    return unjoinedGroups;
+}
+
+}
