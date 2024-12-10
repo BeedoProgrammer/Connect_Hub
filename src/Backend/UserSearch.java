cb497817739bed5e84d963ab21d598eef62c661e
Omar Waleed <omarwaleedelsayed2244@gmail.com>
2024-12-11 00:01:09 +0200

    adjusted User search to avoid redundancies

--- src/Backend/UserSearch.java
+++ src/Backend/UserSearch.java
@@ -11,59 +11,61 @@ import java.util.ArrayList;
  * @author user
  */
 public class UserSearch {
+
     private User CurrentUser;
-  private  ArrayList <User> SystemUsers;
- private   ArrayList<User> SearchResults;
-    public UserSearch (User CurrentUser ,ArrayList<User> SystemUsers)
-    {
-        this.CurrentUser=CurrentUser;
-        this.SystemUsers=SystemUsers;
+    private ArrayList<User> SystemUsers;
+    private ArrayList<User> SearchResults;
+
+    public UserSearch(User CurrentUser, ArrayList<User> SystemUsers) {
+        this.CurrentUser = CurrentUser;
+        this.SystemUsers = SystemUsers;
+        SearchResults = new ArrayList<>();
     }
-  public void DesiredPeople(String searchName) {
-        ArrayList<User> desiredUsers = new ArrayList<>();
+
+    public void DesiredPeople(String searchName) {
         for (User user : SystemUsers) {
-            // Check if the username contains the search term (case-insensitive)
-            if (user.getUsername().toLowerCase().contains(searchName.toLowerCase())) {
-                desiredUsers.add(user);
+            if (user.getUsername().toLowerCase().contains(searchName.toLowerCase())
+                    && !user.getUsername().equalsIgnoreCase(this.CurrentUser.getUsername())) {
+                SearchResults.add(user);
             }
         }
-        SearchResults=desiredUsers;
-      
-    
-  }  
+
+    }
 
     public ArrayList<User> getSearchResults() {
         return SearchResults;
     }
-  
-   public ArrayList<User> getAcceptedFriends() {
+
+    public ArrayList<User> getAcceptedFriends() {
         ArrayList<User> acceptedFriends = new ArrayList<>();
-        for (User user : SystemUsers) {
+        for (User user : SearchResults) {
             if (CurrentUser.getRelationshipStatus(user.getUserId()) == FriendshipStatus.ACCEPTED) {
                 acceptedFriends.add(user);
             }
         }
         return acceptedFriends;
     }
-     public ArrayList<User> getUsersWithNoRelationship() {
+
+    public ArrayList<User> getUsersWithNoRelationship() {
         ArrayList<User> noRelationshipUsers = new ArrayList<>();
-        for (User user : SystemUsers) {
+        for (User user : SearchResults) {
             if (!CurrentUser.hasRelationshipWith(user.getUserId())) {
                 noRelationshipUsers.add(user);
             }
         }
         return noRelationshipUsers;
     }
-     public ArrayList<User> getPendingFriends() {
-    ArrayList<User> pendingFriends = new ArrayList<>();
-    for (User user : SystemUsers) {
-        FriendshipStatus status = CurrentUser.getRelationshipStatus(user.getUserId());
-        if (status != null && 
-           (status == FriendshipStatus.PENDINGSENDER || status == FriendshipStatus.PENDINGRECIEVER)) {
-            pendingFriends.add(user);
+
+    public ArrayList<User> getPendingFriends() {
+        ArrayList<User> pendingFriends = new ArrayList<>();
+        for (User user : SearchResults) {
+            FriendshipStatus status = CurrentUser.getRelationshipStatus(user.getUserId());
+            if (status != null
+                    && (status == FriendshipStatus.PENDINGSENDER || status == FriendshipStatus.PENDINGRECIEVER)) {
+                pendingFriends.add(user);
+            }
         }
+        return pendingFriends;
     }
-    return pendingFriends;
-}
 
 }
