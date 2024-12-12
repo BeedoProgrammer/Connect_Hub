38eb84e836a380c0dfb4c34c67cdd688803ee2c6
BeedoProgrammer <abdelrahmanh38@gmail.com>
2024-12-12 17:48:38 +0200

    Change name to Group and Made only admins can set and Create addUser method

--- src/Backend/Group.java
+++ src/Backend/Group.java
@@ -1,23 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Backend;
 
-/**
- *
- * @author user
- */
+import java.util.*;
+
 public class Group {
-    String name;
-    String Descrption;
-    public Group(String name, String Descrption) {
+    private long groupID;
+    private String name;
+    private String groupPhoto;
+    private String describtion;
+    private ArrayList<Long> users = new ArrayList<>();
+
+    public Group(long groupID, String name, String groupPhoto, String describtion) {
+        this.groupID = groupID;
         this.name = name;
-        this.Descrption = Descrption;
+        this.groupPhoto = groupPhoto;
+        this.describtion = describtion;
     }
+
+    public long getGroupID() {
+        return groupID;
+    }
+
     public String getName() {
         return name;
     }
+
+    public String getGroupPhoto() {
+        return groupPhoto;
+    }
+
+    public String getDescribtion() {
+        return describtion;
+    }
+
+    public void setName(String name, User moderator) {
+        GroupDetails groupRelationStatus = moderator.getGroupRelationStatus(groupID);
+        if(groupRelationStatus == GroupDetails.ADMIN || groupRelationStatus == GroupDetails.CREATOR)
+            this.name = name;
+    }
+
+    public void setGroupPhoto(String groupPhoto, User moderator) {
+        GroupDetails groupRelationStatus = moderator.getGroupRelationStatus(groupID);
+        if(groupRelationStatus == GroupDetails.ADMIN || groupRelationStatus == GroupDetails.CREATOR)
+            this.groupPhoto = groupPhoto;
+    }
+
+    public void setDescribtion(String describtion, User moderator) {
+        GroupDetails groupRelationStatus = moderator.getGroupRelationStatus(groupID);
+        if(groupRelationStatus == GroupDetails.ADMIN || groupRelationStatus == GroupDetails.CREATOR)
+            this.describtion = describtion;
+    }
     
-    
+    public void addUser(User user, User moderator){
+        GroupDetails groupRelationStatus = moderator.getGroupRelationStatus(groupID);
+        if(user.getGroupRelationStatus(groupID) == GroupDetails.USER && (groupRelationStatus == GroupDetails.ADMIN || groupRelationStatus == GroupDetails.CREATOR))
+            users.add(user.getUserId());
+    }
 }
