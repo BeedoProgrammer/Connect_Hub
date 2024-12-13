eaf63486b54d4d3e2fa68cd20a283763cab16140
croissant4495 <abdelrahmanelshabiny@gmail.com>
2024-12-13 16:39:29 +0200

    Adjusted the group window manager

--- src/Groups/GroupWindowManager.java
+++ src/Groups/GroupWindowManager.java
@@ -7,6 +7,7 @@ package Groups;
 import Backend.*;
 import Database.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -17,6 +18,7 @@ public class GroupWindowManager {
     ArrayList<Content> myPosts;
     ArrayList<Content> myStories;
     ArrayList<User> groupMembers;
+    ArrayList<User> pendingMembers;
     Loader dataLoader;
 
     public GroupWindowManager(Group myGroup) {
@@ -24,13 +26,67 @@ public class GroupWindowManager {
         myPosts = new ArrayList<>();
         myStories = new ArrayList<>();
         groupMembers = new ArrayList<>();
+        pendingMembers = new ArrayList<>();
         dataLoader = new Loader();
         
-        
+        load();
+    }
+    
+    public void load(){
+        dataLoader.update();
+        updateMembers();
+        updatePending();
+        updatePosts();
+        updateStories();
+    }
+    
+    public void acceptPending(User acceptedUser){
+        if(acceptedUser.getGroupRelationStatus(myGroup.getGroupID()) == GroupDetails.PENDING){
+            acceptedUser.deleteGroupRelation(myGroup.getGroupID());
+            acceptedUser.addGroupRelation(myGroup.getGroupID(), GroupDetails.USER);
+        }
+    }
+    
+    public void updateMembers(){
+        groupMembers = dataLoader.getMembers(myGroup.getGroupID());
+    }
+    
+    public void updatePending(){
+        pendingMembers.clear();
+        ArrayList<User> allUsers = dataLoader.getUsers();
+        for(User i : allUsers){
+            if(i.getGroupRelationStatus(myGroup.getGroupID()) == GroupDetails.PENDING){
+                pendingMembers.add(i);
+            }
+        }
+    }
+    
+    public void updatePosts(){
+        myPosts.clear();
+        ArrayList<Content> allPosts = dataLoader.getPosts();
+        for(Content i : allPosts){
+            if(i.getGroupID() == myGroup.getGroupID()){
+                myPosts.add(i);
+            }
+        }
+    }
+    
+    public void updateStories(){
+        myStories.clear();
+        ArrayList<Content> allStories = dataLoader.getSories();
+        for(Content i : allStories){
+            if(i.getGroupID() == myGroup.getGroupID()){
+                myStories.add(i);
+            }
+        }
     }
     
     public ArrayList<User> getMembers(){
         return groupMembers;
+    }
+
+    public ArrayList<User> getPending(){
+        return pendingMembers;
     }
     
     public ArrayList<Content> getPosts(){
@@ -70,13 +126,17 @@ public class GroupWindowManager {
             return this.storyDatabase.getStories();
         }
         
-        public ArrayList<User> getUsers(long groupID){
+        public ArrayList<User> getMembers(long groupID){
             ArrayList<Long> userIDs = this.groupDatabase.getGroupFromId(groupID).getUsers();
             ArrayList<User> users = new ArrayList<>();
             for(Long i : userIDs){
                 users.add(userDatabase.getUserFromId(i));
             }
             return users;
+        }
+        
+        public ArrayList<User> getUsers(){
+            return this.userDatabase.getUsers();
         }
         
         public User getCurrentUser(long userID){
